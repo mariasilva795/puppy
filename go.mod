8f48c41e54ac492b582f236ad707d03098639807
Maria Silva <maria_silva@Marias-MacBook-Air.local>
2024-09-01 21:42:58 -0500

    third commit

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/mariasilva795/puppy
 
 go 1.23.0
+
+require github.com/mariasilva795/dog v0.0.0-20240902023214-7aff89bd0fbe // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,15 @@
 package puppy
 
+import "github.com/mariasilva795/dog"
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func Woofsss() string {
+	return "Woof Woof" + dog.WhenGroup("Woof")
 }
